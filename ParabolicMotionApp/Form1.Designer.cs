f79feed755189ff6effb55f09afa70af7b113c07
KirillOrig <dpouldark@mail.ru>
2025-05-30 16:03:36 +0300

    second commit

---
+++ ParabolicMotionApp/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace BaseFigureMotionApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBarSpeed;
+        private System.Windows.Forms.TrackBar trackBarRotation;
+        private System.Windows.Forms.TrackBar trackBarScale;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnReset;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBarSpeed = new System.Windows.Forms.TrackBar();
+            this.trackBarRotation = new System.Windows.Forms.TrackBar();
+            this.trackBarScale = new System.Windows.Forms.TrackBar();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarRotation)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarScale)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // trackBarSpeed
+            // 
+            this.trackBarSpeed.Location = new System.Drawing.Point(630, 30);
+            this.trackBarSpeed.Maximum = 20;
+            this.trackBarSpeed.Minimum = 1;
+            this.trackBarSpeed.Name = "trackBarSpeed";
+            this.trackBarSpeed.Size = new System.Drawing.Size(200, 56);
+            this.trackBarSpeed.TabIndex = 1;
+            this.trackBarSpeed.Value = 5;
+            this.trackBarSpeed.Scroll += new System.EventHandler(this.trackBarSpeed_Scroll);
+            // 
+            // trackBarRotation
+            // 
+            this.trackBarRotation.Location = new System.Drawing.Point(630, 90);
+            this.trackBarRotation.Maximum = 20;
+            this.trackBarRotation.Minimum = 1;
+            this.trackBarRotation.Name = "trackBarRotation";
+            this.trackBarRotation.Size = new System.Drawing.Size(200, 56);
+            this.trackBarRotation.TabIndex = 2;
+            this.trackBarRotation.Value = 5;
+            this.trackBarRotation.Scroll += new System.EventHandler(this.trackBarRotation_Scroll);
+            // 
+            // trackBarScale
+            // 
+            this.trackBarScale.Location = new System.Drawing.Point(630, 150);
+            this.trackBarScale.Maximum = 200;
+            this.trackBarScale.Minimum = 50;
+            this.trackBarScale.Name = "trackBarScale";
+            this.trackBarScale.Size = new System.Drawing.Size(200, 56);
+            this.trackBarScale.TabIndex = 3;
+            this.trackBarScale.Value = 100;
+            this.trackBarScale.Scroll += new System.EventHandler(this.trackBarScale_Scroll);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(630, 220);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(90, 30);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Запуск";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(740, 220);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(90, 30);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Стоп";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(630, 270);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(200, 30);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "Сброс";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(630, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Скорость движения фигуры";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(630, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Скорость вращения фигуры";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(630, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Размер фигуры";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(850, 430);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.trackBarScale);
+            this.Controls.Add(this.trackBarRotation);
+            this.Controls.Add(this.trackBarSpeed);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Эмуляция движения базовой фигуры";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarRotation)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarScale)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ ParabolicMotionApp/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace BaseFigureMotionApp
+{
+    public partial class Form1 : Form
+    {
+        // Таймер движения и обновления состояния анимации
+        private Timer movementTimer;
+
+        // Параметры движения по параболе
+        private double progress;           // Параметр траектории от 0 до 1
+        private int movementDirection;     // 1 – движение вперёд, -1 – обратное движение
+        private double movementSpeed;      // Приращение progress за тик (задается пользователем)
+
+        // Параметры вращения
+        private double rotationAngle;      // Текущий угол вращения (в радианах)
+        private double rotationSpeed;      // Приращение угла (в радианах за тик)
+
+        // Масштабирование фигуры (1.0 – исходный масштаб)
+        private double scaleFactor;
+
+        // Параметры траектории (парабола)
+        // Используется фиксированный базовый отступ trajMargin; effectiveMargin пересчитывается с учетом масштаба.
+        private double trajMargin = 20;
+        private double vertexY;   // Y-координата вершины параболы
+        private double A;         // Коэффициент параболы
+        private double midX;      // Центр области по X
+
+        // Центр фигуры (вычисляется по траектории)
+        private PointF figureCenter;
+
+        // Параметр базовой фигуры (измерение "a")
+        // По фото: внешний контур звезды – вписан в окружность радиуса 2a, центральный круг – радиуса a.
+        private float a = 30; // базовая величина в пикселях
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeSimulation();
+        }
+
+        // Пересчитывает параметры траектории с учетом текущего масштаба.
+        private void RecalculateTrajectoryParameters()
+        {
+            // effectiveMargin гарантирует, что расстояние от центра фигуры до границ не меньше 2a*scaleFactor.
+            double effectiveMargin = Math.Max(trajMargin, 2 * a * scaleFactor);
+            double Ybottom = pictureBox1.Height - effectiveMargin;
+            midX = pictureBox1.Width / 2.0;
+            // Выбираем вершину так, чтобы не касаться верхней границы – примерно на 1/3 расстояния от верха до низа.
+            vertexY = effectiveMargin + (Ybottom - effectiveMargin) / 3.0;
+            double d = midX - effectiveMargin;
+            A = (Ybottom - vertexY) / (d * d);
+        }
+
+        private void InitializeSimulation()
+        {
+            progress = 0.0;
+            movementDirection = 1;
+            rotationAngle = 0.0;
+            // Значения скоростей берутся из TrackBar’ов
+            movementSpeed = trackBarSpeed.Value / 1000.0;
+            rotationSpeed = trackBarRotation.Value * Math.PI / 180.0;
+            scaleFactor = trackBarScale.Value / 100.0; // 100 → масштаб 1.0
+
+            // Пересчитываем параметры траектории с учетом текущего масштаба
+            RecalculateTrajectoryParameters();
+
+            // Начальная позиция фигуры — вычисляем по траектории при progress = 0.
+            double effectiveMargin = Math.Max(trajMargin, 2 * a * scaleFactor);
+            double xPos = effectiveMargin + progress * (pictureBox1.Width - 2 * effectiveMargin);
+            double yPos = A * ((xPos - midX) * (xPos - midX)) + vertexY;
+            figureCenter = new PointF((float)xPos, (float)yPos);
+
+            // Настройка таймера движения (около 50 тиков в секунду)
+            movementTimer = new Timer();
+            movementTimer.Interval = 20;
+            movementTimer.Tick += MovementTimer_Tick;
+        }
+
+        private void MovementTimer_Tick(object sender, EventArgs e)
+        {
+            // Обновляем параметры скоростей (на случай изменений пользователем)
+            movementSpeed = trackBarSpeed.Value / 1000.0;
+            rotationSpeed = trackBarRotation.Value * Math.PI / 180.0;
+
+            progress += movementSpeed * movementDirection;
+
+            // Если достигнут конец траектории – сразу меняем направление движения и вращения
+            if (progress >= 1.0)
+            {
+                progress = 1.0;
+                movementDirection = -1;
+            }
+            else if (progress <= 0.0)
+            {
+                progress = 0.0;
+                movementDirection = 1;
+            }
+
+            // Обновляем угол вращения (направление вращения синхронизировано с направлением движения)
+            rotationAngle += rotationSpeed * movementDirection;
+
+            // Пересчитываем параметры траектории с учетом масштаба
+            double effectiveMargin = Math.Max(trajMargin, 2 * a * scaleFactor);
+            double xPos = effectiveMargin + progress * (pictureBox1.Width - 2 * effectiveMargin);
+            double yPos = A * ((xPos - midX) * (xPos - midX)) + vertexY;
+            figureCenter = new PointF((float)xPos, (float)yPos);
+
+            pictureBox1.Invalidate();
+        }
+
+        // Функция линейной интерполяции цвета по коэффициенту t (0 ≤ t ≤ 1)
+        private Color InterpolateColor(Color start, Color end, double t)
+        {
+            int r = (int)(start.R + (end.R - start.R) * t);
+            int g = (int)(start.G + (end.G - start.G) * t);
+            int b = (int)(start.B + (end.B - start.B) * t);
+            return Color.FromArgb(r, g, b);
+        }
+
+        // Вычисление локальных координат точек пятиконечной звезды, центрированной в (0, 0)
+        private PointF[] GetStarPoints()
+        {
+            PointF[] points = new PointF[10];
+            double angle = -Math.PI / 2; // начинаем с -90° (верхняя точка)
+            double angleStep = Math.PI / 5; // 36° между точками
+            double R = 2 * a;         // внешний радиус – 2a
+            double r = 0.382 * (2 * a); // внутренний радиус (≈0.764a)
+            for (int i = 0; i < 10; i++)
+            {
+                double radius = (i % 2 == 0) ? R : r;
+                float x = (float)(radius * Math.Cos(angle));
+                float y = (float)(radius * Math.Sin(angle));
+                points[i] = new PointF(x, y);
+                angle += angleStep;
+            }
+            return points;
+        }
+
+        // Применяем преобразование к точкам: вращение, масштабирование и перенос (translation) с учётом figureCenter
+        private PointF[] TransformPoints(PointF[] pts)
+        {
+            PointF[] transformed = new PointF[pts.Length];
+            float cos = (float)Math.Cos(rotationAngle);
+            float sin = (float)Math.Sin(rotationAngle);
+            for (int i = 0; i < pts.Length; i++)
+            {
+                // Вращение
+                float xRot = pts[i].X * cos - pts[i].Y * sin;
+                float yRot = pts[i].X * sin + pts[i].Y * cos;
+                // Масштабирование
+                xRot *= (float)scaleFactor;
+                yRot *= (float)scaleFactor;
+                // Перенос
+                transformed[i] = new PointF(xRot + figureCenter.X, yRot + figureCenter.Y);
+            }
+            return transformed;
+        }
+
+        // Отрисовка фигуры: внешняя пятиконечная звезда и центральный круг.
+        // Цвет звезды интерполируется от синего (progress = 0) до зелёного (progress = 1),
+        // а цвет круга – от жёлтого до синего.
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+
+            Color starColor = InterpolateColor(Color.Blue, Color.Green, progress);
+            Color circleColor = InterpolateColor(Color.Yellow, Color.Blue, progress);
+
+            // Рисуем звезду
+            PointF[] starLocal = GetStarPoints();
+            PointF[] starTransformed = TransformPoints(starLocal);
+            using (SolidBrush brushStar = new SolidBrush(starColor))
+            {
+                g.FillPolygon(brushStar, starTransformed);
+            }
+
+            // Рисуем центральный круг
+            float circleRadius = a * (float)scaleFactor; // радиус центрального круга = a * scaleFactor
+            RectangleF circleRect = new RectangleF(
+                figureCenter.X - circleRadius,
+                figureCenter.Y - circleRadius,
+                2 * circleRadius,
+                2 * circleRadius);
+            using (SolidBrush brushCircle = new SolidBrush(circleColor))
+            {
+                g.FillEllipse(brushCircle, circleRect);
+            }
+        }
+
+        // Обработчики элементов управления:
+
+        // Кнопка "Запуск" – старт анимации
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            movementTimer.Start();
+        }
+
+        // Кнопка "Стоп" – остановка анимации
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            movementTimer.Stop();
+        }
+
+        // Кнопка "Сброс" – возврат к исходному состоянию
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            movementTimer.Stop();
+
+            progress = 0.0;
+            movementDirection = 1;
+            rotationAngle = 0.0;
+            scaleFactor = 1.0;
+            trackBarScale.Value = 100;
+
+            RecalculateTrajectoryParameters();
+            double effectiveMargin = Math.Max(trajMargin, 2 * a * scaleFactor);
+            double xPos = effectiveMargin + progress * (pictureBox1.Width - 2 * effectiveMargin);
+            double yPos = A * ((xPos - midX) * (xPos - midX)) + vertexY;
+            figureCenter = new PointF((float)xPos, (float)yPos);
+
+            pictureBox1.Invalidate();
+        }
+
+        // Изменение масштаба через TrackBar: обновляем scaleFactor, пересчитываем траекторию и перерисовываем фигуру
+        private void trackBarScale_Scroll(object sender, EventArgs e)
+        {
+            scaleFactor = trackBarScale.Value / 100.0;
+            RecalculateTrajectoryParameters();
+            double effectiveMargin = Math.Max(trajMargin, 2 * a * scaleFactor);
+            double xPos = effectiveMargin + progress * (pictureBox1.Width - 2 * effectiveMargin);
+            double yPos = A * ((xPos - midX) * (xPos - midX)) + vertexY;
+            figureCenter = new PointF((float)xPos, (float)yPos);
+            pictureBox1.Invalidate();
+        }
+
+        // При изменении скорости или вращения значения обновляются в каждом тике таймера
+        private void trackBarSpeed_Scroll(object sender, EventArgs e) { }
+        private void trackBarRotation_Scroll(object sender, EventArgs e) { }
+
+        // При изменении размеров формы пересчитываем параметры траектории, чтобы фигура оставалась внутри рабочей области
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (pictureBox1 != null)
+            {
+                RecalculateTrajectoryParameters();
+                double effectiveMargin = Math.Max(trajMargin, 2 * a * scaleFactor);
+                double xPos = effectiveMargin + progress * (pictureBox1.Width - 2 * effectiveMargin);
+                double yPos = A * ((xPos - midX) * (xPos - midX)) + vertexY;
+                figureCenter = new PointF((float)xPos, (float)yPos);
+                pictureBox1.Invalidate();
+            }
+        }
+    }
+}
